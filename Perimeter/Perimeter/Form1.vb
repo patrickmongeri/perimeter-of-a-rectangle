3292257aa87a1299b377ffab3331115919ca199c
patrickmongeri <79586567+patrickmongeri@users.noreply.github.com>
2021-02-25 16:40:30 +0300

    Add files via upload

---
+++ Perimeter/Perimeter/Form1.vb
@@ -0,0 +1,58 @@
+﻿'Ali Ahmad
+'CP Period 3
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim intSide1 As UInteger = 0
+        Dim intSide2 As UInteger = 0
+        Dim intSide3 As UInteger = 0
+        Dim intSide4 As UInteger = 0
+
+        Try
+            intSide1 = Convert.ToUInt32(TextBox1.Text)
+            intSide2 = Convert.ToUInt32(TextBox2.Text)
+            intSide3 = Convert.ToUInt32(TextBox3.Text)
+            intSide4 = Convert.ToUInt32(TextBox4.Text)
+        Catch ex As Exception
+            lblMain.Text = "Please enter correct numeric values."
+            MessageBox.Show("Incorrect values", "Error")
+        End Try
+
+        Dim intPerimeter As UInteger = intSide1 + intSide2 + intSide3 + intSide4
+        lblMain.Text = intPerimeter
+
+        If intSide1 > intSide3 Then
+            lblMain.Text = "Side's 1 and 3 must be equal"
+            MessageBox.Show("Improper rectangle", "Error")
+        ElseIf intSide2 > intSide4 Then
+            lblMain.Text = "Side's 2 and 4 must be equal"
+            MessageBox.Show("Improper rectangle", "Error")
+        ElseIf intSide1 < intSide3 Then
+            lblMain.Text = "Side's 1 and 3 must be equal"
+            MessageBox.Show("Improper rectangle", "Error")
+        ElseIf intSide2 < intSide4 Then
+            lblMain.Text = "Side's 2 and 4 must be equal"
+            MessageBox.Show("Improper rectangle", "Error")
+        End If
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        lblMain.Text = ""
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+        lblMain.Text = ""
+    End Sub
+
+    Private Sub TextBox3_TextChanged(sender As Object, e As EventArgs) Handles TextBox3.TextChanged
+        lblMain.Text = ""
+    End Sub
+
+    Private Sub TextBox4_TextChanged(sender As Object, e As EventArgs) Handles TextBox4.TextChanged
+        lblMain.Text = ""
+    End Sub
+End Class
+
+
---
+++ Perimeter/Perimeter/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Perimeter")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Perimeter")>
+<Assembly: AssemblyCopyright("Copyright ©  2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("d1eb2b7a-759f-4aca-9c7e-1b98c82a3ce2")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
